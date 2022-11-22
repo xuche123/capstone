f982b5197bebe6af4386bd7e3ac09685565758de
Xu Che <xuche1998@gmail.com>
2022-11-22 20:56:01 +0800

    add spinner

--- imagine/static/imagine/generate.js
+++ imagine/static/imagine/generate.js
@@ -1,17 +1,21 @@
 document.addEventListener("DOMContentLoaded", function () {
   document.querySelector("#generate").onclick = function () {
+    var spinner = document.querySelector(".spinner-border");
+    spinner.style.display = "block";
     var text = document.querySelector("#prompt").value;
     console.log(text);
+
     fetch("/generate_prompt", {
       // Fetch request
       method: "POST",
       body: JSON.stringify({
         prompt: text,
       }),
-    }) // Fetch response to JSON object
+    })
       .then((response) => response.json())
       .then((result) => {
         // Print result
+        spinner.style.display = "none";
         const img = document.querySelector("#image");
         img.src = result.url;
       });
