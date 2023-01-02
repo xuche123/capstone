8c08b1a8733e50009dfc8f63ceefb87ffec9cb47
xuche123 <xuche1998@gmail.com>
2023-01-02 11:15:51 +0800

    change darkmode button

--- imagine/static/imagine/darkMode.js
+++ imagine/static/imagine/darkMode.js
@@ -9,8 +9,12 @@ document.addEventListener("DOMContentLoaded", function () {
   function toggleDarkMode(state) {
     if (state) {
       html.setAttribute("data-bs-theme", "dark");
+      button.classList.add("bi-moon-stars-fill")
+      button.classList.remove("bi-sun-fill")
     } else {
       html.setAttribute("data-bs-theme", "light");
+      button.classList.remove("bi-moon-stars-fill")
+      button.classList.add("bi-sun-fill")
     }
 
     darkModeState = state;
